a9b2180e5f5f2794ba3c5c3a7f240850c1ac6d4b
punkyoon <cryptography@konkuk.ac.kr>
2017-07-31 23:03:34 +0900

    developing...

--- knock_knock_bot/urls.py
+++ knock_knock_bot/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from server import views
+
 urlpatterns = [
+    url(r'^keyboard/', views.keyboard),
+    url(r'message', views.message),
     url(r'^admin/', admin.site.urls),
 ]
--- server/views.py
+++ server/views.py
@@ -1,3 +1,29 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+def keyboard(request):
+    data = {
+        'type': 'buttons',
+        'buttons': ['1', '2']
+    }
+    return JsonResponse(json.dumps(data, ensure_ascii=False), safe=False)
+
+@csrf_exempt
+def message(request):
+    print(request.body)
+
+    data = {
+        'message': {
+            'text': 'hello world'
+        },
+        'keyboard': {
+            'type': 'buttons',
+            'buttons': ['1', '2']
+        }
+    }
+
+    return JsonResponse(json.dumps(data, ensure_ascii=False), safe=False)
