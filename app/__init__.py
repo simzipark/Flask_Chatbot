ded7a0e6afcbfd9fc15fae3b824bba90c8d074e1
bzh0406@naver.com <cryptography@konkuk.ac.kr>
2017-08-15 03:58:27 +0000

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from collections import defaultdict
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 session = defaultdict()
